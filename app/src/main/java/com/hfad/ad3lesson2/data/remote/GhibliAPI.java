fe2e4de47e8ce4cc450b2f81f168c3311ccd017a
CyberN0mad <71142523+CyberN0mad@users.noreply.github.com>
2021-03-04 23:47:40 +0600

    HW2

---
+++ app/src/main/java/com/hfad/ad3lesson2/data/remote/GhibliAPI.java
@@ -0,0 +1,28 @@
+package com.hfad.ad3lesson2.data.remote;
+
+import com.hfad.ad3lesson2.data.model.Film;
+import com.hfad.ad3lesson2.data.model.People;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GhibliAPI {
+
+
+
+    @GET(EndPoints.END_POINTS)
+    Call<List<Film>> getFilms();
+
+    @GET(EndPoints.END_POINTS_ID)
+    Call<Film> getSpecFilm(@Path("id") String id
+    );
+
+    @GET(EndPoints.END_POINTS_PEOPLE)
+    Call<List<People>> getPeople();
+
+    @GET(EndPoints.END_POINTS_PEOPLE_BY_ID)
+    Call<People> getPeopleById(@Path("id") String id);
+}
